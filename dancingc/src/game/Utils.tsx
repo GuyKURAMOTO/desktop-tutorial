3c907f4e473db4b363e139c18c467c2b034dd695
GuyKURAMOTO <g.kurarymond@gmail.com>
2022-04-27 10:56:19 +0900

    add util functions

---
+++ dancingc/src/game/Utils.tsx
@@ -0,0 +1,17 @@
+export function shuffle<T>(array: T[]) {
+  const out = Array.from(array);
+  for (let i = out.length - 1; i > 0; i--) {
+    const r = Math.floor(Math.random() * (i + 1));
+    const tmp = out[i];
+    out[i] = out[r];
+    out[r] = tmp;
+  }
+  return out;
+};
+
+export function chunk<T extends any[]>(arr: T, size: number) {
+  return arr.reduce(
+    (newarr, _, i) => (i % size ? newarr : [...newarr, arr.slice(i, i + size)]),
+    [] as T[][]
+  )
+};
